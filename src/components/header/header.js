395cc8df33b1a029b9803492756cbdefbea04e88
dogrod <rodrickjue@hotmail.com>
2017-07-26 15:02:57 +0800

    feat: complete my first react component

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h2>My first react app</h2>
+      </div>
+    )
+  }
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Header from './components/header/header.js'
+// import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Header />, 
+  document.getElementById('root')
+)
+// registerServiceWorker()
