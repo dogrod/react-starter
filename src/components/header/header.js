511362e5ca435e8248e1167a9aef898021e3977e
dogrod <rodrickjue@hotmail.com>
2017-07-26 15:51:28 +0800

    feat: split dynamic function to class property

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react'
 import './header.css'
 
 export default class Header extends Component {
-  render() {
-    const isWelecomePage = false
-    const className = 'header'
+  renderContent() {
+    const isWelecomePage = true
     const name = 'dogrod'
+
+    return isWelecomePage
+            ? `Hello, ${name}'s first react app`
+            : `Goodbye ${name}`
+  }
+
+  render() {
+    const className = 'header'
     return (
       <div className={className}>
-        {isWelecomePage
-          ? <h2>{name}'s first react app</h2>
-          : <h2>Goodbye {name}</h2>
-        }
+        <h2>
+          {this.renderContent()}
+        </h2>
       </div>
     )
   }
