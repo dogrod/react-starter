02cec46fe9d8a8bd1d3f2a29897553d2ae943c98
dogrod <rodrickjue@hotmail.com>
2017-07-26 15:31:24 +0800

    feat: use variable for dynamic display content

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,9 +3,15 @@ import './header.css'
 
 export default class Header extends Component {
   render() {
+    const isWelecomePage = false
+    const className = 'header'
+    const name = 'dogrod'
     return (
-      <div className="header">
-        <h2>My first react app</h2>
+      <div className={className}>
+        {isWelecomePage
+          ? <h2>{name}'s first react app</h2>
+          : <h2>Goodbye {name}</h2>
+        }
       </div>
     )
   }
